14a9c983112664dd23b81b2324ff6bf83fa7c3fb
haneulee <lovesky4294@gmail.com>
2018-10-18 18:03:42 +0900

    add test code

---
+++ alarm.js
@@ -0,0 +1,19 @@
+var Alarm = (function(){
+    // function Singleton(args) {
+        
+    //     var args = args || {};
+    //     this.a = args.a;
+    //     this.b = args.b;  
+    // }
+
+    // var INSTANCE;
+
+    return {        
+        // getInstance: function ( args ){
+        //     if (INSTANCE === undefined) {
+        //         instance = new Singleton( args );
+        //     }
+        //     return INSTANCE;
+        // }
+    };
+})();
---
+++ tests.js
@@ -0,0 +1,3 @@
+QUnit.test( "hello test", function( assert ) {
+    assert.equal( 1, 1, "Passed!" );
+  });
