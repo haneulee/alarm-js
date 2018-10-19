de6c20b1a7072706f8e6cb8b9e8ab1a55fb6114c
haneulee <lovesky4294@gmail.com>
2018-10-19 17:13:38 +0900

    alarm spec

--- alarm.js
+++ alarm.js
@@ -1,38 +1,130 @@
 var Alarm = (function() {
   var alarmTimer = null,
     curTimeEl = document.getElementById("curTime"),
-    setTimeEl,
-    currentTime,
-    alarmSet;
+    listEl = document.getElementById("list"),
+    currentTime;
 
   function initAlarm(e) {
     currentTime = currentTime || new Date();
 
     if (alarmTimer != null) clearInterval(alarmTimer);
-
-    clearAlarm();
-
+    
     document.alarmForm.h.value = currentTime.getHours();
     document.alarmForm.m.value = currentTime.getMinutes();
-    // document.alarmForm.s.value = currentTime.getSeconds();
     document.alarmForm.add.addEventListener("click", function() {
       setAlarm();
     });
+    document.alarmForm.clear.addEventListener("click", function() {
+      clearAlarm();
+    });
     document.alarmForm.setTime.addEventListener("click", function() {
       setTime();
+    });
+    listEl.addEventListener("click", function(e) {
+      if (e.target.name === "snooze") {
+        snoozeAlarm(e.target);
+      } else if (e.target.name === "delete") {
+        deleteAlarm(e.target);
+      }
     });
 
     alarmTimer = setInterval(function() {
       countTime();
     }, 1000);
+
+    updateAlarm();
   }
 
   function setAlarm() {
     alarmSet = true;
+
+    if (isNaN(document.alarmForm.h.value) 
+        || isNaN(document.alarmForm.m.value) 
+        || !document.alarmForm.content.value) {
+        window.alert("값을 입력해주세요!");
+        return;
+    }
+
+    var alarmStorage = localStorage.getItem("alarm"),
+      newAlarm = {
+        id: '_' + Math.random().toString(36).substr(2, 9),
+        msg: document.alarmForm.content.value,
+        time: document.alarmForm.h.value * 3600 + document.alarmForm.m.value * 60, //currentTime || new Date(),
+        snooze: false,
+        alarmMode: document.alarmForm.alarmMode.value,
+        clockMode: document.alarmForm.clockMode.value
+      };
+
+    if (!alarmStorage) {
+      localStorage.setItem("alarm", JSON.stringify([newAlarm]));
+    } else {
+      var curList = JSON.parse(alarmStorage);
+      localStorage.setItem("alarm", JSON.stringify(curList.concat([newAlarm])));
+    }
+
+    updateAlarm();
+
+    document.alarmForm.h.value = "";
+    document.alarmForm.m.value = "";
+    document.alarmForm.content.value = "";
+  }
+
+  function updateAlarm() {
+    var alarmList = localStorage.getItem("alarm"),
+      i, time, h, m, color;
+
+    if (!alarmList) {
+      return;
+    }
+
+    listEl.innerHTML = "";
+    alarmList = JSON.parse(alarmList);
+
+    alarmList.sort((a, b) => a.time > b.time);
+
+    for (i = 0; i < alarmList.length; i++) {
+      time = alarmList[i].time;
+      h = Math.floor(time / 3600);
+      m = Math.floor(time % 3600 / 60);
+      color = alarmList[i].snooze ? 'style="color:green;"' : "";
+      
+      listEl.innerHTML += '<li id="' + alarmList[i].id + '"><span>' + (h < 10 ? "0" : "") + h + ':' + (m < 10 ? "0" : "") +  m + ' </span><span>' + alarmList[i].msg 
+      + '</span><input type=button name=snooze ' + color + ' value="끄기"><input type=button name=delete value="삭제"></li>';
+    }
+
+  }
+
+  function deleteAlarm(target) {
+    var targetId = target.parentNode.id,
+      alarmList = JSON.parse(localStorage.getItem("alarm"));
+
+    alarmList = alarmList.filter(function(item) {
+      return item.id !== targetId
+    });
+
+    localStorage.setItem("alarm", JSON.stringify(alarmList));
+    updateAlarm();
+  }
+
+  function snoozeAlarm(target) {
+    var targetId = target.parentNode.id,
+      alarmList = JSON.parse(localStorage.getItem("alarm")),
+      i;
+
+    for (i = 0; i < alarmList.length; i++) {
+      if (alarmList[i].id == targetId) {
+        alarmList[i].snooze = !(alarmList[i].snooze);
+        target.style.color = alarmList[i].snooze === true ? "green" : "black";
+         break;
+      }
+    }
+
+    localStorage.setItem("alarm", JSON.stringify(alarmList));
   }
 
   function clearAlarm() {
-    alarmSet = false;
+    localStorage.removeItem("alarm");
+    listEl.innerHTML = "";
   }
 
   function setTime() {
@@ -44,33 +136,42 @@ var Alarm = (function() {
       document.alarmForm.sm.value,
       document.alarmForm.ss.value
     );
-  }
 
-  function matchH() {
-    return document.alarmForm.ch.value == document.alarmForm.h.value;
-  }
-
-  function matchM() {
-    return document.alarmForm.cm.value == document.alarmForm.m.value;
-  }
-
-  function matchS() {
-    // return document.alarmForm.cs.value == document.alarmForm.s.value;
+    document.alarmForm.sh.value = "";
+    document.alarmForm.sm.value = "";
+    document.alarmForm.ss.value = "";
   }
 
   function countTime() {
     var s = currentTime.getSeconds(),
       m = currentTime.getMinutes(),
       h = currentTime.getHours(),
-      time;
+      alarmList, i, time, ah, am;
 
     s += 1;
 
+    //분이 바뀔 때
     if (s == 60) {
       s = 0;
       m += 1;
+
+      //알람 검사
+      if (localStorage.getItem("alarm")) {
+        alarmList = JSON.parse(localStorage.getItem("alarm"));
+
+        for (i = 0; i < alarmList.length; i++) {
+          time = alarmList[i].time;
+          ah = Math.floor(time / 3600);
+          am = Math.floor(time % 3600 / 60);
+
+          if (m == am && h == ah) {
+            popupAlarm(alarmList[i], ah, am);
+          }
+        }
+      }
     }
 
+    //시가 바뀔 때
     if (m == 60) {
       m = 0;
       h += 1;
@@ -93,10 +194,31 @@ var Alarm = (function() {
     document.alarmForm.ch.value = currentTime.getHours();
     document.alarmForm.cm.value = currentTime.getMinutes();
     document.alarmForm.cs.value = currentTime.getSeconds();
+  }
 
-    if (matchH() && matchM() && matchS()) {
-      alert("뚜뚜뚜뚜...일어나세요... ");
+  function popupAlarm(itemObj, h, m) {
+    var alarmType = "", 
+      text;
+
+    if (itemObj.snooze) {
+      return;
     }
+
+    if (itemObj.clockMode === "일반") {
+      alarmType = "소리";
+    } else if (itemObj.clockMode === "진동") {
+      alarmType = "진동";
+    } else {
+      if (itemObj.alarmMode === "긴급") {
+        alarmType = "소리";
+      } else {
+        return;
+      }
+    }
+
+    text = h + ' : ' + m + '\n ' + itemObj.msg + '\n ' + alarmType + '입니다!!';
+
+    window.alert(text);
   }
 
   return {
